2297b0d8edf4ce380ba6ecf2061d88ea0816efcb
chensheng233 <chensheng233@outlook.com>
2019-10-03 17:07:13 +0800

    进一步完善

--- src/main/java/com/juku/reflection/TypeResolver.java
+++ src/main/java/com/juku/reflection/TypeResolver.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.Arrays;
 
+
 /**
  * 获取泛型参数类型要注意下面这些点：
  * 当一个方法返回值包含泛型或参数列表包含泛型。这个时候声明这些方法或参数值的接口和类可以是 ParameterizedType 或 Class
@@ -27,37 +28,49 @@ public abstract class TypeResolver
 		Type returnType = method.getGenericReturnType();
 		
 		if(returnType instanceof TypeVariable) {
-			return resolveForVar((TypeVariable<?>)returnType,clazz,declaringClass);
+			return resolveTypeVariable((TypeVariable<?>)returnType,clazz,declaringClass);
+		}else if(returnType instanceof ParameterizedType) {
+			return resolveParameterizedType((ParameterizedType)returnType,clazz,declaringClass);
 		}
 		return null;
 	}
 	
 
-	private static Type resolveForVar(TypeVariable<?> typeVar,Class<?> srcClass,Class<?> declaringClass) {
+	private static Type resolveTypeVariable(TypeVariable<?> typeVar,Type srcType,Class<?> declaringClass) {
+		Class<?> srcClass = null;
+		if(srcType instanceof Class) {
+			srcClass = (Class<?>)srcType;
+		}
 		if(srcClass == declaringClass) {
-			Type srcType = srcClass;
 			if(srcType instanceof TypeVariable) {
 				Type[] bounds = typeVar.getBounds();
 			      if (bounds.length > 0) {
 			        return bounds[0];
 			      }
 			      return Object.class;
+			}else if(srcType instanceof ParameterizedType) {
+				ParameterizedType parentAsType = (ParameterizedType) srcType;
+				Class<?> parentRawType = (Class<?>) parentAsType.getRawType();
+				TypeVariable<?>[] rawTypes = parentRawType.getTypeParameters();
+				for(int i=0;i<rawTypes.length;i++) {
+					if(typeVar == rawTypes[i]) {
+						return parentAsType.getActualTypeArguments()[i];
+					}
+				}				
 			}
-			ParameterizedType parentAsType = (ParameterizedType) srcType;
-			Class<?> parentRawType = (Class<?>) parentAsType.getRawType();
-			TypeVariable<?>[] rawTypes = parentRawType.getTypeParameters();
-			for(int i=0;i<rawTypes.length;i++) {
-				if(typeVar == rawTypes[i]) {
-					return parentAsType.getActualTypeArguments()[i];
-				}
-			}
+			
+			
+		}
+		Type superClass = null;
+		if(srcClass!=null) {
+			superClass = srcClass.getGenericSuperclass();
+		}else {
+			superClass = srcType;
 		}
 		
-		Type superClass = srcClass.getGenericSuperclass();
 		if(superClass instanceof ParameterizedType) {
 			ParameterizedType parentAsType = (ParameterizedType) superClass;
 			Class<?> parentRawType = (Class<?>) parentAsType.getRawType();
-			
 			TypeVariable<?>[] rawTypes = parentRawType.getTypeParameters();
 			if(parentRawType == declaringClass) {
 				for(int i=0;i<rawTypes.length;i++) {
@@ -66,13 +79,69 @@ public abstract class TypeResolver
 					}
 				}				
 			}else {
-				return resolveForVar(typeVar,(Class<?>)superClass,declaringClass);
+				return resolveTypeVariable(typeVar,(Class<?>)superClass,declaringClass);
 			}
 
 		}else if(superClass instanceof Class) {
-			return resolveForVar(typeVar,(Class<?>)superClass,declaringClass);
+			return resolveTypeVariable(typeVar,(Class<?>)superClass,declaringClass);
 		}
 		return Object.class;
+	}
+
+
+	public static ParameterizedType resolveParameterizedType(ParameterizedType typeVar,Type srcType,Class<?> declaringClass) {
+		Class<?> srcClass = (Class<?>)srcType;
+		if(srcClass == declaringClass) {
+			return typeVar;
+		}
+		
+		Type superType = srcClass.getGenericSuperclass();		
+		
+		// 返回泛型参数泛型变量数组
+		Type[] returnTypes = typeVar.getActualTypeArguments();
+		Type[] result = new Type[returnTypes.length];
+		for(int i=0;i<result.length;i++) {
+			if(returnTypes[i] instanceof TypeVariable) {
+				result[i] = resolveTypeVariable((TypeVariable<?>)returnTypes[i],superType,declaringClass);
+			}else if(returnTypes[i] instanceof ParameterizedType) {
+				result[i] = resolveParameterizedType((ParameterizedType)returnTypes[i],superType,declaringClass);
+			}
+		}
+		
+		Class<?> rawClass = (Class<?>)typeVar.getRawType();
+		return new ParameterizedTypeImple(result,rawClass,null);
+	}
+	
+	private static class ParameterizedTypeImple implements ParameterizedType{
+		private Type[] actualTypeArguments;
+		private Type rawType;
+		private Type owerType;
+		public ParameterizedTypeImple(Type[] actualTypeArguments, Type rawType, Type owerType) {
+			super();
+			this.actualTypeArguments = actualTypeArguments;
+			this.rawType = rawType;
+			this.owerType = owerType;
+		}
+		@Override
+		public Type[] getActualTypeArguments() {
+			// TODO Auto-generated method stub
+			return actualTypeArguments;
+		}
+		@Override
+		public Type getRawType() {
+			// TODO Auto-generated method stub
+			return rawType;
+		}
+		@Override
+		public Type getOwnerType() {
+			// TODO Auto-generated method stub
+			return owerType;
+		}
+		@Override
+		public String toString() {
+			return "ParameterizedTypeImple [actualTypeArguments=" + Arrays.toString(actualTypeArguments) + ", rawType="
+					+ rawType + ", owerType=" + owerType + "]";
+		}		
 	}
 
 	
